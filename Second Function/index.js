8faf83420cf35ef28bc9629429ccd896e2772816
AM-EO <evgenameo@gmail.com>
2021-01-26 15:18:57 +0300

    Second commit

---
+++ Second Function/index.js
@@ -0,0 +1,18 @@
+/*function mySecondFunction(name) {
+    let message = 'Привет, ' + name + '!';
+    return message;
+}
+
+var myMessage = mySecondFunction();
+
+alert(myMessage);*/
+
+
+function mySecondFunction(name) {
+    let message = 'Привет, ' + name + '!';
+    return message;
+}
+
+var myMessage = mySecondFunction('Женя');
+
+alert(myMessage);
